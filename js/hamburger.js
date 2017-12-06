bfa741f3c9d16b8e5d0a39989cdd1f6a205da376
Jared East <JaredEastPhoto@gmail.com>
2017-12-05 22:46:06 -0500

    Hamburger Plug in

---
+++ js/hamburger.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+
+$( ".cross" ).hide();
+$( ".menu2" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu2" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu2" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
+});
