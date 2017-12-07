0e2354b635739589f2d263334191243c2fca809d
Jared East <JaredEastPhoto@gmail.com>
2017-12-06 23:46:24 -0500

    custom jquery

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
- 
+
+//custom jquery//
+$(document).ready(function () {
+
+    $('.button').click(function () {
+
+        alert('Due to the our extraordinary customization options please call 386-233-5356 and make an appointment with us, Thank you!');
+    }
+    );
+});
